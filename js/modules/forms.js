ea0cd5ef2f21bd2130ea93954aac28965ae3fe4e
Dmitry <dkoltakov97@gamil.com>
2020-09-07 12:23:55 +0300

    first commit

---
+++ js/modules/forms.js
@@ -0,0 +1,75 @@
+import {closeModal, openModal} from './modal';//вызываем в одном модуле кусочек другого модуля
+//функций опен и клоз модал тут просто не было
+//но переменной modal просто не существует для этого файла
+//поэтому оригинальную функцию нужно подправить чтобы она получала как аргумент селекторы и передавала их в переменную modal
+
+import {postData} from "../services/services"; // функцию пост дата мы вынесли в services и тут ее принимаем
+
+function forms(formSelector, modalTimerId) {
+    // Forms
+
+    const forms = document.querySelectorAll(formSelector);
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        bindPostData(item);
+    });
+
+    function bindPostData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+        
+            const formData = new FormData(form);
+
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal('.modal', modalTimerId);
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal('.modal');
+        }, 4000);
+    }
+}
+
+export default forms;
